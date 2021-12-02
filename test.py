154a7290c0b3350473c5869732ae698651c8a3af
Jungu Lee <heback@gmail.com>
2021-12-02 22:05:31 +0900

    Update test.py

--- test.py
+++ test.py
@@ -58,3 +58,14 @@ st.dataframe(df.style.highlight_max(axis=0))
 	
 st.write("st.table api")
 st.table(df)
+
+with st.form("my_form"):
+  st.write("Inside the form")
+  slider_val = st.slider("Form slider")
+  checkbox_val = st.checkbox("Form checkbox")
+  # Every form must have a submit button.
+  submitted = st.form_submit_button("Submit")
+  if submitted:
+    st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.write("Outside the form")
