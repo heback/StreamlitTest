71ccdb29822ef50d2e64de662b6932c693584d0f
Jungu Lee <heback@gmail.com>
2021-12-02 16:10:28 +0900

    Update test.py

--- test.py
+++ test.py
@@ -33,3 +33,9 @@ st.write('Values:', values)
 
 title = st.text_input('Movie title', 'Life of Brian')
 st.write('The current movie title is', title)
+
+start_color, end_color = st.select_slider(
+  'Select a range of color wavelength',
+  options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
+  value=('red', 'blue'))
+st.write('You selected wavelengths between', start_color, 'and', end_color)
